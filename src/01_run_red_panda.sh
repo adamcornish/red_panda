d2e43e178d69ffbdc912b4d47df52ca99b52da49
adambioi <cornish.adam@gmail.com>
2018-11-20 17:30:11 -0600

    Update 01_run_red_panda.sh

--- src/01_run_red_panda.sh
+++ src/01_run_red_panda.sh
@@ -1,4 +1,4 @@
 dir=/path/to/output/dir
 sample=example
 genome=mm10
-perl red_panda.pl -v $dir/$sample.vcf -s $sample -d data -g $genom -V
+perl red_panda.pl -v $dir/$sample.vcf -s $sample -d data -g $genome -V
