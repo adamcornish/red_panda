2b0077159f4f5d40cd2e763b39a497fb5376615b
adambioi <cornish.adam@gmail.com>
2018-12-16 18:13:58 -0600

    Add files via upload

--- src/01_run_red_panda.sh
+++ src/01_run_red_panda.sh
@@ -1,4 +1,5 @@
 dir=/path/to/output/dir
 sample=example
 genome=mm10
-perl red_panda.pl -v $dir/$sample.vcf -s $sample -d data -g $genome -V
+annotation=/path/to/genome/annotation.gtf
+perl red_panda.pl -v $dir/$sample.vcf -s $sample -d data -g $genome -A $annotation -V
